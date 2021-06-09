576f85d8e7c76d1d9e27a2f09f7e113e9fa63e92
mAdkins <software@doorways.org>
2021-06-09 15:19:47 -0700

    Push file to create directory in github repo

---
+++ test/doc.go
@@ -0,0 +1,4 @@
+// Package test provides resources to tests in other packages.
+// All files in this package must be named ending in _test.go
+// in order to prevent them from being added to compiled code.
+package test
