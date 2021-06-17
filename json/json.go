2d5b38e52fe25bc29b02af00e99009d37591b5d4
mAdkins <software@doorways.org>
2021-06-17 08:23:57 -0700

    Remove old JSON code and rename lab files

--- json/json.go
+++ json/json.go
@@ -1,61 +1,49 @@
 package json
 
 import (
-	"bufio"
 	"encoding/json"
 	"fmt"
-	"io"
-	"regexp"
 	"strings"
 
 	"github.com/madkins23/go-type/reg"
-	"github.com/madkins23/go-type/serial"
 )
 
-func NewConverter(mapper serial.Mapper) serial.Converter {
-	return serial.NewConverter(&conversion{}, mapper)
+type Wrapper struct {
+	TypeName string
+	Contents json.RawMessage
 }
 
-//////////////////////////////////////////////////////////////////////////
-
-type conversion struct {
-}
-
-var typeMatcher = regexp.MustCompile("\"" + reg.TypeFieldEscaped + "\":\\s*\"([^\"]+)\",")
-
-func (c conversion) TypeName(reader io.ReadSeeker) (string, error) {
-	buffered := bufio.NewReader(reader)
-
-	for {
-		if line, _, err := buffered.ReadLine(); err == io.EOF {
-			break
-		} else if err != nil {
-			return "", fmt.Errorf("read line: %w", err)
-		} else if matches := typeMatcher.FindStringSubmatch(string(line)); len(matches) < 1 {
-			continue
-		} else if _, err := reader.Seek(0, io.SeekStart); err != nil {
-			return "", fmt.Errorf("reset reader: %w", err)
-		} else {
-			// Trim off any quotes and whitespace.
-			return strings.Trim(matches[1], "'\" "), nil
-		}
+func (w *Wrapper) Wrap(item interface{}) error {
+	var err error
+	w.TypeName, err = reg.NameFor(item)
+	if err != nil {
+		return fmt.Errorf("get type name for %#v: %w", item, err)
 	}
 
-	return "", fmt.Errorf("unable to locate type field")
-}
-
-func (c conversion) Decode(item interface{}, reader io.Reader) error {
-	if err := json.NewDecoder(reader).Decode(item); err != nil {
-		return fmt.Errorf("decode item from JSON: %w", err)
-	}
-	return nil
-}
-
-func (c conversion) Encode(item interface{}, writer io.Writer) error {
-	encoder := json.NewEncoder(writer)
+	build := &strings.Builder{}
+	encoder := json.NewEncoder(build)
 	encoder.SetEscapeHTML(false)
-	if err := encoder.Encode(item); err != nil {
-		return fmt.Errorf("encode item to JSON: %w", err)
+	if err = encoder.Encode(item); err != nil {
+		return fmt.Errorf("marshal wrapper contents: %w", err)
 	}
+	w.Contents = []byte(build.String())
+
 	return nil
+}
+
+func (w *Wrapper) Unwrap() (interface{}, error) {
+	if w.TypeName == "" {
+		return nil, fmt.Errorf("empty type field")
+	} else if item, err := reg.Make(w.TypeName); err != nil {
+		return nil, fmt.Errorf("make instance of type %s: %w", w.TypeName, err)
+	} else if err = json.NewDecoder(strings.NewReader(string(w.Contents))).Decode(item); err != nil {
+		return nil, fmt.Errorf("decode wrapper contents: %w", err)
+	} else {
+		return item, nil
+	}
+}
+
+func WrapItem(item interface{}) (*Wrapper, error) {
+	w := &Wrapper{}
+	return w, w.Wrap(item)
 }
--- json/json_test.go
+++ json/json_test.go
@@ -1,57 +1,42 @@
 package json
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"strings"
+	"os"
+	"strconv"
 	"testing"
 
-	"github.com/madkins23/go-type/reg"
-	"github.com/madkins23/go-type/serial"
-
+	"github.com/davecgh/go-spew/spew"
 	"github.com/stretchr/testify/suite"
 
+	"github.com/madkins23/go-type/reg"
 	"github.com/madkins23/go-type/test"
 )
 
-var _ serial.Mappable = &filmJson{}
-
-// testMapper must be global to be accessible from filmJSON.
-// Normally this would be provided by serial.Highlander(),
-// but for testing purposes create it globally so it uses the local Registry object.
-var testMapper serial.Mapper
+// Trial and error approach to simplifying things.
 
 type JsonTestSuite struct {
 	suite.Suite
-	film      *filmJson
-	registry  reg.Registry
-	mapper    serial.Mapper
-	converter serial.Converter
+	showAccount bool
 }
 
 func (suite *JsonTestSuite) SetupSuite() {
-	suite.registry = reg.NewRegistry()
-	suite.Assert().NotNil(suite.registry)
-	suite.mapper = serial.NewMapper(suite.registry)
-	suite.Assert().NotNil(suite.mapper)
-	suite.Assert().NoError(suite.registry.AddAlias("test", test.Alpha{}), "creating test alias")
-	suite.Assert().NoError(suite.registry.AddAlias("testJSON", &filmJson{}), "creating testJSON alias")
-	suite.Assert().NoError(suite.registry.Register(&filmJson{}))
-	suite.Assert().NoError(suite.registry.Register(&test.Alpha{}))
-	suite.Assert().NoError(suite.registry.Register(&test.Bravo{}))
+	if showAccount, found := os.LookupEnv("GO-TYPE-SHOW-ACCOUNT"); found {
+		var err error
+		suite.showAccount, err = strconv.ParseBool(showAccount)
+		suite.Require().NoError(err)
+	}
+	suite.showAccount = true
+	suite.Require().NoError(reg.AddAlias("test", test.Account{}), "creating test alias")
+	suite.Require().NoError(reg.Register(&test.Stock{}))
+	suite.Require().NoError(reg.Register(&test.Bond{}))
+	suite.Require().NoError(reg.AddAlias("jsonTest", Account{}), "creating test alias")
+	suite.Require().NoError(reg.Register(&Account{}))
 }
 
 func (suite *JsonTestSuite) SetupTest() {
-	suite.converter = NewConverter(suite.mapper)
-	suite.Assert().NotNil(suite.converter)
-	suite.film = &filmJson{Name: "Test JSON", Index: make(map[string]test.Actor)}
-	suite.film.Lead = &test.Alpha{Name: "Goober", Percent: 13.23}
-	suite.film.addActor("Goober", suite.film.Lead)
-	suite.film.addActor("Snoofus", test.NewBravo(false, 17, "stuff"))
-	suite.film.addActor("Noodle", test.NewAlpha("Noodle", 19.57, "stuff"))
-	suite.film.addActor("Soup", &test.Bravo{Finished: true, Iterations: 79})
-	testMapper = suite.mapper // no other convenient way to pass to where it's needed
 }
 
 func TestJsonSuite(t *testing.T) {
@@ -60,293 +45,200 @@ func TestJsonSuite(t *testing.T) {
 
 //////////////////////////////////////////////////////////////////////////
 
-// TestExample duplicates the YAML test.
-// TODO: Not directly applicable to this test suite.
-func (suite *JsonTestSuite) TestExample() {
-	type T struct {
-		F int `json:"a,omitempty"`
-		B int
-	}
-	t := T{F: 1, B: 2}
-	bytes, err := json.Marshal(t)
-	suite.Assert().NoError(err)
-	var x T
-	suite.Assert().NoError(json.Unmarshal(bytes, &x))
-	suite.Assert().Equal(t, x)
-}
-
-//////////////////////////////////////////////////////////////////////////
-
-func (suite *JsonTestSuite) TestConverterIsRegistry() {
-	_, ok := suite.converter.(reg.Registry)
-	suite.Assert().True(ok)
-}
-
-func (suite *JsonTestSuite) TestGetTypeName() {
-	reader := strings.NewReader(simpleJson)
-	suite.Assert().NotNil(reader)
-	typeName, err := suite.converter.TypeName(reader)
-	suite.Assert().NoError(err)
-	suite.Assert().Equal("[testJSON]filmJson", typeName)
-}
+//// TestExample duplicates the YAML test.
+//// TODO: Not directly applicable to this test suite.
+//func (suite *JsonTestSuite) TestExample() {
+//	type T struct {
+//		F int `json:"a,omitempty"`
+//		B int
+//	}
+//	t := T{F: 1, B: 2}
+//	bytes, err := json.Marshal(t)
+//	suite.Assert().NoError(err)
+//	var x T
+//	suite.Assert().NoError(json.Unmarshal(bytes, &x))
+//	suite.Assert().Equal(t, x)
+//}
 
 //////////////////////////////////////////////////////////////////////////
 
 // TestMarshalCycle verifies the JSON Marshal/Unmarshal works as expected.
 func (suite *JsonTestSuite) TestMarshalCycle() {
-	bytes, err := json.Marshal(suite.film)
-	suite.Assert().NoError(err)
+	account := MakeAccount()
 
-	//fmt.Printf(">>> marshaled:\n%#v\n", string(bytes))
-
-	var film filmJson
-	suite.Assert().NoError(json.Unmarshal(bytes, &film))
-	suite.Assert().NotEqual(suite.film, &film) // fails due to unexported field 'extra'
-	for _, act := range suite.film.Cast {
-		// Remove unexported field.
-		if a, ok := act.(*test.Alpha); ok {
-			a.ClearExtra()
-		} else if b, ok := act.(*test.Bravo); ok {
-			b.ClearExtra()
-		}
+	marshaled, err := json.Marshal(account)
+	suite.Require().NoError(err)
+	if suite.showAccount {
+		var buf bytes.Buffer
+		suite.Require().NoError(json.Indent(&buf, marshaled, "", "  "))
+		fmt.Println(buf.String())
 	}
-	suite.Assert().Equal(suite.film, &film) // succeeds now that unexported fields are gone.
+	suite.Assert().Contains(string(marshaled), "TypeName")
+	suite.Assert().Contains(string(marshaled), "[test]Stock")
+	suite.Assert().Contains(string(marshaled), "[test]Bond")
+
+	var newAccount Account
+	suite.Require().NoError(json.Unmarshal(marshaled, &newAccount))
+	if suite.showAccount {
+		fmt.Println("---------------------------")
+		spew.Dump(newAccount)
+	}
+
+	suite.Assert().NotEqual(account, newAccount)
+	account.Favorite.ClearPrivateFields()
+	for _, position := range account.Positions {
+		position.ClearPrivateFields()
+	}
+	for _, position := range account.Lookup {
+		position.ClearPrivateFields()
+	}
+	// Succeeds now that unexported (private) fields are gone.
+	suite.Assert().Equal(account, &newAccount)
 }
 
-func (suite *JsonTestSuite) TestLoadFromString() {
-	item, err := suite.converter.LoadFromString(simpleJson)
-	suite.Assert().NoError(err)
-	suite.Assert().NotNil(item)
-}
-
-func (suite *JsonTestSuite) TestSaveToString() {
-	text, err := suite.converter.SaveToString(suite.film)
-	suite.Assert().NoError(err)
-	suite.Assert().NotEmpty(text)
-	suite.Assert().True(strings.Contains(text, `"<type>":"[testJSON]filmJson"`))
-	suite.Assert().True(strings.Contains(text, `"<type>":"[test]Alpha"`))
-	suite.Assert().True(strings.Contains(text, `"<type>":"[test]Bravo"`))
-}
-
-func (suite *JsonTestSuite) TestMarshalFileCycle() {
-	file, err := ioutil.TempFile("", "*.test.json")
-	suite.Assert().NoError(err)
-	suite.Assert().NotNil(file)
-	fileName := file.Name()
-	// Go ahead and close it, just needed the file name.
-	suite.Assert().NoError(file.Close())
-
-	film := suite.makeTestFilm()
-	suite.Assert().NoError(suite.converter.SaveToFile(film, fileName))
-
-	reloaded, err := suite.converter.LoadFromFile(fileName)
-	suite.Assert().NoError(err)
-	suite.Assert().NotNil(reloaded)
-	// TODO: Fix!
-	suite.Assert().Equal(film, reloaded)
-}
-
-func (suite *JsonTestSuite) TestMarshalStringCycle() {
-	film := suite.makeTestFilm()
-	str, err := suite.converter.SaveToString(film)
-	suite.Assert().NoError(err)
-	suite.NotZero(str)
-
-	fmt.Print("--- TestMarshalStringCycle ---------\n", str, "------------------------------------\n")
-
-	reloaded, err := suite.converter.LoadFromString(str)
-	suite.Assert().NoError(err)
-	suite.Assert().NotNil(reloaded)
-	// TODO: Fix!
-	suite.Assert().EqualValues(film, reloaded)
-}
+//func (suite *JsonTestSuite) TestLoadFromString() {
+//	item, err := suite.converter.LoadFromString(simpleJson)
+//	suite.Assert().NoError(err)
+//	suite.Assert().NotNil(item)
+//}
+//
+//func (suite *JsonTestSuite) TestSaveToString() {
+//	text, err := suite.converter.SaveToString(suite.film)
+//	suite.Assert().NoError(err)
+//	suite.Assert().NotEmpty(text)
+//	suite.Assert().True(strings.Contains(text, `"<type>":"[testJSON]filmJson"`))
+//	suite.Assert().True(strings.Contains(text, `"<type>":"[test]Alpha"`))
+//	suite.Assert().True(strings.Contains(text, `"<type>":"[test]Bravo"`))
+//}
+//
+//func (suite *JsonTestSuite) TestMarshalFileCycle() {
+//	file, err := ioutil.TempFile("", "*.test.json")
+//	suite.Assert().NoError(err)
+//	suite.Assert().NotNil(file)
+//	fileName := file.Name()
+//	// Go ahead and close it, just needed the file name.
+//	suite.Assert().NoError(file.Close())
+//
+//	film := suite.makeTestFilm()
+//	suite.Assert().NoError(suite.converter.SaveToFile(film, fileName))
+//
+//	reloaded, err := suite.converter.LoadFromFile(fileName)
+//	suite.Assert().NoError(err)
+//	suite.Assert().NotNil(reloaded)
+//	// TODO: Fix!
+//	suite.Assert().Equal(film, reloaded)
+//}
+//
+//func (suite *JsonTestSuite) TestMarshalStringCycle() {
+//	film := suite.makeTestFilm()
+//	str, err := suite.converter.SaveToString(film)
+//	suite.Assert().NoError(err)
+//	suite.NotZero(str)
+//
+//	fmt.Print("--- TestMarshalStringCycle ---------\n", str, "------------------------------------\n")
+//
+//	reloaded, err := suite.converter.LoadFromString(str)
+//	suite.Assert().NoError(err)
+//	suite.Assert().NotNil(reloaded)
+//	// TODO: Fix!
+//	suite.Assert().EqualValues(film, reloaded)
+//}
 
 //////////////////////////////////////////////////////////////////////////
 
-func (suite *JsonTestSuite) makeTestFilm() *filmJson {
-	actor1 := &test.Alpha{
-		Name:    "Goober Snoofus",
-		Percent: 13.23,
-	}
-	actor2 := &test.Bravo{
-		Finished:   true,
-		Iterations: 1957,
-	}
-	return &filmJson{
-		Name: "Blockbuster",
-		Lead: actor1,
-		Cast: []test.Actor{
-			actor1,
-			actor2,
-		},
-		Index: map[string]test.Actor{
-			"Snoofus, Goober": actor1,
-			"Snarly, Booger":  actor2,
-		},
+func MakeAccount() *Account {
+	account := &Account{}
+	account.MakeFake()
+	return account
+}
+
+type Account struct {
+	test.Account
+}
+
+type xferAccount struct {
+	Account struct {
+		Favorite  *Wrapper
+		Positions []*Wrapper
+		Lookup    map[string]*Wrapper
 	}
 }
 
-//////////////////////////////////////////////////////////////////////////
+func (a *Account) MarshalJSON() ([]byte, error) {
+	xfer := &xferAccount{}
 
-type filmJson struct {
-	json.Marshaler
-	json.Unmarshaler
-
-	Name  string
-	Lead  test.Actor
-	Cast  []test.Actor
-	Index map[string]test.Actor
-}
-
-func (film *filmJson) addActor(name string, act test.Actor) {
-	film.Cast = append(film.Cast, act)
-	film.Index[name] = act
-}
-
-const fldName = "name"
-const fldLead = "lead"
-const fldCast = "cast"
-const fldIndex = "index"
-
-func (film *filmJson) Marshal() (map[string]interface{}, error) {
+	// Wrap objects referenced by interface fields.
 	var err error
-
-	converted := make(map[string]interface{})
-
-	converted[fldName] = film.Name
-
-	if converted[fldLead], err = testMapper.Marshal(film.Lead); err != nil {
-		return nil, fmt.Errorf("converting %s to map: %w", fldLead, err)
-	}
-
-	cast := make([]interface{}, len(film.Cast))
-	for i, member := range film.Cast {
-		if cast[i], err = testMapper.Marshal(member); err != nil {
-			return nil, fmt.Errorf("converting %s member to map: %w", fldCast, err)
+	if a.Favorite != nil {
+		if xfer.Account.Favorite, err = WrapItem(a.Favorite); err != nil {
+			return nil, fmt.Errorf("wrap favorite: %w", err)
 		}
 	}
-	converted[fldCast] = cast
-
-	index := make(map[string]interface{}, len(film.Index))
-	for key, member := range film.Index {
-		if index[key], err = testMapper.Marshal(member); err != nil {
-			return nil, fmt.Errorf("converting cast member to map: %w", err)
-		}
-	}
-	converted[fldIndex] = index
-
-	return converted, nil
-}
-
-func (film *filmJson) Unmarshal(mapData map[string]interface{}) error {
-	var ok bool
-	var err error
-	if film.Name, ok = mapData[fldName].(string); !ok {
-		return fmt.Errorf("bad film name")
-	} else if film.Name == "" {
-		return fmt.Errorf("no film name")
-	}
-
-	if film.Lead, err = film.unmarshalActor(mapData[fldLead]); err != nil {
-		return fmt.Errorf("unmarshaling lead actor: %w", err)
-	}
-
-	if cast, ok := mapData[fldCast].([]interface{}); !ok {
-		return fmt.Errorf("bad cast")
-	} else {
-		film.Cast = make([]test.Actor, len(cast))
-		for i, member := range cast {
-			if film.Cast[i], err = film.unmarshalActor(member); err != nil {
-				return fmt.Errorf("unmarshaling cast member: %w", err)
+	if a.Positions != nil {
+		fixed := make([]*Wrapper, len(a.Positions))
+		for i, pos := range a.Positions {
+			if fixed[i], err = WrapItem(pos); err != nil {
+				return nil, fmt.Errorf("wrap Positions item: %w", err)
 			}
 		}
+		xfer.Account.Positions = fixed
 	}
-
-	if index := mapData[fldIndex].(map[string]interface{}); !ok {
-		return fmt.Errorf("bad index")
-	} else {
-		film.Index = make(map[string]test.Actor, len(index))
-		for name, member := range index {
-			if film.Index[name], err = film.unmarshalActor(member); err != nil {
-				return fmt.Errorf("unmarshaling index member: %w", err)
+	if a.Lookup != nil {
+		fixed := make(map[string]*Wrapper, len(a.Lookup))
+		for k, pos := range a.Lookup {
+			if fixed[k], err = WrapItem(pos); err != nil {
+				return nil, fmt.Errorf("wrap Lookup item: %w", err)
 			}
 		}
+		xfer.Account.Lookup = fixed
+	}
+
+	return json.Marshal(xfer)
+}
+
+func (a *Account) UnmarshalJSON(marshaled []byte) error {
+	xfer := &xferAccount{}
+	if err := json.Unmarshal(marshaled, xfer); err != nil {
+		return fmt.Errorf("unmarshal to loader: %w", err)
+	}
+
+	// Unwrap objects referenced by interface fields.
+	var err error
+	if a.Favorite, err = a.getInvestment(xfer.Account.Favorite); err != nil {
+		return fmt.Errorf("get Investment from Favorite")
+	}
+	if xfer.Account.Positions != nil {
+		fixed := make([]test.Investment, len(xfer.Account.Positions))
+		for i, wPos := range xfer.Account.Positions {
+			if fixed[i], err = a.getInvestment(wPos); err != nil {
+				return fmt.Errorf("get Investment from Positions")
+			}
+		}
+		a.Positions = fixed
+	}
+	if xfer.Account.Lookup != nil {
+		fixed := make(map[string]test.Investment, len(xfer.Account.Lookup))
+		for key, wPos := range xfer.Account.Lookup {
+			if fixed[key], err = a.getInvestment(wPos); err != nil {
+				return fmt.Errorf("get Investment from Lookup")
+			}
+		}
+		a.Lookup = fixed
 	}
 
 	return nil
 }
 
-// MarshalJSON is called to marshal the filmJson object properly.
-// It is necessary filmJson contains some interface fields that must be populated.
-func (film *filmJson) MarshalJSON() ([]byte, error) {
-	if toMap, err := film.Marshal(); err != nil {
-		return nil, fmt.Errorf("pushing film to map: %w", err)
-	} else {
-		return json.Marshal(toMap)
-	}
-}
-
-func (film *filmJson) UnmarshalJSON(input []byte) error {
-	var err error
-	mapData := make(map[string]interface{})
-	if err = json.Unmarshal(input, &mapData); err != nil {
-		return fmt.Errorf("unmarshal JSON to map data: %w", err)
+func (a *Account) getInvestment(w *Wrapper) (test.Investment, error) {
+	var ok bool
+	var investment test.Investment
+	if w != nil {
+		if item, err := w.Unwrap(); err != nil {
+			return nil, fmt.Errorf("unwrap item: %w", err)
+		} else if investment, ok = item.(test.Investment); !ok {
+			return nil, fmt.Errorf("item %#v not Investment", item)
+		}
 	}
 
-	return film.Unmarshal(mapData)
+	return investment, nil
 }
-
-func (film *filmJson) unmarshalActor(input interface{}) (test.Actor, error) {
-	actMap, ok := input.(map[string]interface{})
-	if !ok {
-		return nil, fmt.Errorf("actor input should be map")
-	} else if item, err := testMapper.Unmarshal(actMap); err != nil {
-		return nil, fmt.Errorf("creating item from map: %w", err)
-	} else if act, ok := item.(test.Actor); !ok {
-		return nil, fmt.Errorf("item is not an actor")
-	} else {
-		return act, nil
-	}
-}
-
-//////////////////////////////////////////////////////////////////////////
-
-const simpleJson = `{
-    "<type>": "[testJSON]filmJson",
-    "name":   "Blockbuster Movie",
-    "lead": {
-        "<type>": "[test]Alpha",
-        "name": "Lance Lucky",
-        "percent": 23.79,
-        "extra": "Yaaaa!"
-    },
-    "cast": [
-        {
-            "<type>": "[test]Alpha",
-            "name": "Lance Lucky",
-            "percent": 23.79,
-            "extra": false
-        },
-        {
-            "<type>": "[test]Bravo",
-            "finished": true,
-            "iterations": 13,
-            "extra": "gibbering ghostwhistle"
-        }
-    ],
-    "index": {
-        "Lucky, Lance": {
-            "<type>": "[test]Alpha",
-            "name": "Lance Lucky",
-            "percent": 23.79,
-            "extra": "marshmallow stars"
-        },
-        "Queue, Susie": {
-            "<type>": "[test]Bravo",
-            "finished": true,
-            "iterations": 13,
-            "extra": 19.57
-        }
-    }
-}
-`
